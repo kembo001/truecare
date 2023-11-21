01bdf4b329420653a6eb5835d147d8ffff555a76
Brandon <brandonkemboi87@gmail.com>
2023-11-21 13:33:10 -0600

    New Content

--- src/components/pages/About/PageIntro.js
+++ src/components/pages/About/PageIntro.js
@@ -25,12 +25,10 @@ function PageIntro() {
         {" "}
         <div className="feature__content">
           {" "}
-          <h2 className="content__title">Sample Heading Text</h2>{" "}
+          <h2 className="content__title">About Us</h2>{" "}
           <p className="content__text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum magni
-            aliquid, a necessitatibus iusto tempora pariatur hic itaque iste
-            consequatur dolorum assumenda quibusdam dolore deleniti laudantium
-            laborum sed asperiores beatae.
+          Welcome to TrueCare, where we transcend the role of a traditional healthcare provider to become your trusted partners in wellness and compassion. Committed to delivering exceptional home health and hospice care, we have dedicated ourselves to serving our community with unwavering dedication.
+
           </p>{" "}
 <div class="content__buttons"> <a href="#" class="button button-primary" title="">CTA Button Text</a> <a href="#" class="button button-secondary" title="">BUTTON TEXT</a> </div>
         </div>{" "}
--- src/components/pages/About/ServicesDescription.js
+++ src/components/pages/About/ServicesDescription.js
@@ -10,20 +10,33 @@ function ServicesDescription() {
   <div className="content-area col-12">
 
             <div id="dnn_ctr434_HtmlModule_lblContent" className="Normal">
-              <h2>Hospice Services You Can Count On</h2>{" "}
+              <h2>Our Philosophy</h2>{" "}
               <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. At deserunt corrupti voluptatum vitae consequuntur hic explicabo provident reiciendis dolor voluptate rem deleniti, aperiam, minima dolorum laudantium. Sed aliquam voluptas eius?
+              At TrueCare, our philosophy is grounded in a simple yet profound belief – every individual deserves to age with dignity, comfort, and the highest quality of care, all within the familiarity of their own home. We believe in making health and well-being an integral part of your daily life, seamlessly woven into the fabric of your home environment.
+
               </p>{" "}
+              <h3>Our Commitment
+</h3><p>For a significant period, we have been at the forefront of providing outstanding home health and hospice services. Our experience reflects a deep understanding of the unique needs and challenges that individuals and their families face during health-related transitions.
+</p>
+           
+              <h2>Our Mission</h2>
               <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. At deserunt corrupti voluptatum vitae consequuntur hic explicabo provident reiciendis dolor voluptate rem deleniti, aperiam, minima dolorum laudantium. Sed aliquam voluptas eius?
+              At the heart of our mission is a dedication to enhancing the lives of those we serve. We understand the unique challenges individuals and their families face when dealing with health issues, and we are driven to make a difference. Our mission is to provide personalized, patient-centered care that not only addresses medical needs but also fosters emotional support, hope, and a sense of belonging.
+
               </p>{" "}
-              <h3>Have your Love ones in good care</h3>{" "}
+              <h2>Tailored Care, Comprehensive Services:
+</h2>
               <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. At deserunt corrupti voluptatum vitae consequuntur hic explicabo provident reiciendis dolor voluptate rem deleniti, aperiam, minima dolorum laudantium. Sed aliquam voluptas eius?
-              </p>{" "}
-              <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. At deserunt corrupti voluptatum vitae consequuntur hic explicabo provident reiciendis dolor voluptate rem deleniti, aperiam, minima dolorum laudantium. Sed aliquam voluptas eius?
+              TrueCare is proud to offer a comprehensive range of home health and hospice services tailored to the diverse needs of our community. From skilled nursing and rehabilitation to emotional support and hospice care, our dedicated team of professionals is here to provide the highest standard of care with a personal touch.
+
               </p>
+              <h2>Why TrueCare</h2>
+              <ul>
+<li>Experienced Professionals: Our team comprises skilled and compassionate professionals with extensive backgrounds in home healthcare and hospice services.</li>
+<li>Community Focus: Deeply rooted in the community, we prioritize understanding and addressing the unique needs of our neighbors.</li>
+<li>Dignity at the Core: We uphold the principle that every individual deserves to age with dignity, and our commitment reflects this fundamental belief.</li>
+</ul><p>Choose TrueCare as your partner in compassion, where your well-being takes center stage. Together, let's embark on the journey to better health and a more fulfilling life, right in the heart of your home.
+</p>
             </div>
 
         <div className="clear" />
